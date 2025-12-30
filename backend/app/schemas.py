ea2752d4b53146fb210b92f3cd08994abb86d360
ian-sequ7 <iansequeira@icloud.com>
2025-12-29 16:45:01 -0800

    commit schemas fro back to front end response

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,9 +1,42 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
+    # how a paper is seen from front
 class PaperRespose(BaseModel):
     id: int
     title: str
     year: Optional[int]
     venue: Optional[str]
-    citation_count: Optional[int]   
+    citation_count: Optional[int]  
+
+    class Config:
+        from_attributes =True
+
+class FacultyResponse(BaseModel):
+    id: int
+    name: str
+    affiliation: Optional[str]
+    h_index: Optional[int]
+    paper_count: Optional[int]
+    semantic_scholar_id: optional[int]
+
+    class Config
+        from_attributes = True
+
+class SearchResult(BaseModel):
+    faculty: FacultyResponse
+    similarity: float # similarity basically means a higher # means a better match
+    papers: list[PaperResponse] = []
+    
+class SearchRequest(Basemodel):
+    query: str
+    limit: int = 10
+    min_h_index: int = 0
+
+class ExplanationRequest(BaseModel):
+    interests: str
+    faculty_id: str
+
+class ExplanationResponse(BaseModel):
+    explanation: str
