960149e3d63f5a555ec09251f4412ee81f63b7ea
ian-sequ7 <iansequeira@icloud.com>
2026-01-17 23:11:01 -0800

    commit better number of faculty
    expanded ds, new econ/metrics, etc

--- backend/scripts/fetch_faculty.py
+++ backend/scripts/fetch_faculty.py
@@ -470,12 +470,318 @@ MATH_FACULTY = {
     ]
 }
 
+# --- GENERAL MATHEMATICS (Probability, Combinatorics, Analysis) ---
+MATH_GENERAL_FACULTY = {
+    "MIT Math": [
+        "Alexei Borodin", "Jacopo Borga", "Elchanan Mossel", "Philippe Rigollet",
+        "Scott Sheffield", "Nike Sun"
+    ],
+    "Stanford Math": [
+        "Daniel Bump", "Persi Diaconis", "Jacob Fox", "Sarah Peluse",
+        "Kannan Soundararajan", "Jan Vondrák", "Sourav Chatterjee"
+    ],
+    "Berkeley Math": [
+        "Bernd Sturmfels", "Lauren Williams", "Michael Christ", "Tatiana Toro",
+        "James Sethian", "Martin Wainwright"
+    ],
+    "Princeton Math": [
+        "Noga Alon", "Maria Chudnovsky", "Peter Constantin", "Charles Fefferman",
+        "Igor Rodnianski", "Paul Seymour", "Allan Sly", "Elias Stein"
+    ],
+    "UChicago Math": [
+        "László Babai", "Gregory Lawler", "Ewain Gwynne", "Alexander Razborov", "Amie Wilkinson"
+    ],
+    "Harvard Math": [
+        "Laura DeMarco", "Denis Auroux", "Joe Harris", "Lauren Williams",
+        "Mark Kisin", "Hiro Lee Tanaka"
+    ],
+    "Yale Math": [
+        "Ronald Coifman", "Peter Jones", "Richard Kenyon", "Wilhelm Schlag",
+        "Charles Smart", "Van Vu"
+    ],
+    "Columbia Math": [
+        "Ivan Corwin", "Julien Dubedat", "Ioannis Karatzas", "Marcel Nutz",
+        "Daniel Lacker", "Andrei Okounkov"
+    ],
+    "Cornell Math": [
+        "Marcelo Aguiar", "Robert Connelly", "Allen Knutson", "Lionel Levine",
+        "Karola Meszaros", "Edward Swartz", "Tara Holm"
+    ],
+    "NYU Courant Math": [
+        "Gerard Ben Arous", "Paul Bourgade", "Percy Deift", "Sinan Gunturk",
+        "Nina Holden", "Eyal Lubetzky", "Charles Newman", "S. R. Srinivasa Varadhan",
+        "Ofer Zeitouni", "Sylvia Serfaty"
+    ],
+    "UCLA Math": [
+        "Igor Pak", "Terence Tao", "Marek Biskup", "John Garnett",
+        "Rowan Killip", "Sorin Popa", "Dimitri Shlyakhtenko", "Raphael Rouquier"
+    ],
+    "UMich Math": [
+        "Jeff Lagarias", "Kartik Prasanna", "Mattias Jonsson", "Stephen DeBacker", "Karen Smith"
+    ],
+    "Wisconsin Math": [
+        "David Anderson", "Hanbaek Lyu", "Hao Shen", "Benedek Valko",
+        "Paul Terwilliger", "Botong Wang", "Timo Seppalainen"
+    ],
+    "UPenn Math": [
+        "James Haglund", "Philip Gressman", "Ryan Hynd", "Yumeng Ou",
+        "Robin Pemantle", "Robert Strain", "Jason Altschuler"
+    ],
+    "Duke Math": [
+        "Nicholas Cook", "Alexander Dunlap", "Richard Durrett", "Jianfeng Lu",
+        "Jonathan Mattingly", "James Nolen", "Ezra Miller", "Alexander Kiselev"
+    ],
+    "Northwestern Math": [
+        "Xiumin Du", "Reza Gheissari", "Rachel Greenfeld", "Elton Hsu",
+        "Marcus Michelen", "Bryna Kra", "Sandy Zabell"
+    ],
+    "Caltech Math": [
+        "David Conlon", "Thomas Hutchcroft", "Nikolai Markarov", "Eric Rains",
+        "Omer Tamuz", "Lingfu Zhang", "Matilde Marcolli"
+    ],
+    "Brown Math": [
+        "Brendan Hassett", "Dan Abramovich", "Jill Pipher", "Brett Bernstein"
+    ],
+    "UT Austin Math": [
+        "Luis Caffarelli", "Francesco Maggi", "Matias Delgadino", "Irene Gamba",
+        "Maria Gualdani", "Arie Israel", "Alexis Vasseur"
+    ],
+    "CMU Math": [
+        "Alan Frieze", "Tom Bohman", "Boris Bukh", "Po-Shen Loh",
+        "Prasad Tetali", "Wesley Pegden"
+    ]
+}
+
+# --- PHYSICS (Quantum Information, Statistical Physics, Condensed Matter Theory) ---
+PHYSICS_FACULTY = {
+    "MIT Physics": [
+        "Mehran Kardar", "Isaac Chuang", "William Oliver", "Aram Harrow",
+        "Paola Cappellaro", "Liang Fu", "Max Metlitski", "Xiao-Gang Wen",
+        "Leonid Mirny", "Jesse Thaler", "Max Tegmark", "Soonwon Choi"
+    ],
+    "Stanford Physics": [
+        "Patrick Hayden", "Steven Kivelson", "Vedika Khemani", "Xiaoliang Qi",
+        "Monika Schleier-Smith", "Shanhui Fan", "Jelena Vuckovic", "Ben Feldman"
+    ],
+    "Berkeley Physics": [
+        "Hartmut Häffner", "Norman Yao", "Joel Moore", "Michael Zaletel",
+        "Jeffrey Neaton", "Irfan Siddiqi", "Dung-Hai Lee", "Steven Louie"
+    ],
+    "Princeton Physics": [
+        "Andrew Houck", "Jason Petta", "Herman Verlinde", "Silviu Pufu",
+        "Jeff Thompson", "Ali Yazdani"
+    ],
+    "UChicago Physics": [
+        "David Awschalom", "Cheng Chin", "Sidney Nagel", "Suriyanarayanan Vaikuntanathan",
+        "Promit Ghosal"
+    ],
+    "Harvard Physics": [
+        "Mikhail Lukin", "Ashvin Vishwanath", "Subir Sachdev", "Markus Greiner",
+        "Efthimios Kaxiras", "Eugene Khalaf"
+    ],
+    "Yale Physics": [
+        "Michel Devoret", "Robert Schoelkopf", "Steven Girvin", "Daisuke Nagai"
+    ],
+    "Columbia Physics": [
+        "Andrew Millis", "Henry Yuen", "Dmitri Basov", "Abhay Pasupathy",
+        "Sebastian Will", "Raquel Queiroz", "Dries Sels"
+    ],
+    "Cornell Physics": [
+        "Eun-Ah Kim", "Paul McEuen", "Michelle Wang", "Nick Rivera"
+    ],
+    "Caltech Physics": [
+        "John Preskill", "Alexei Kitaev", "Fernando Brandao", "Oskar Painter",
+        "Manuel Endres", "Xie Chen"
+    ],
+    "UCLA Physics": [
+        "Prineha Narang", "Alexander Balandin", "Yaroslav Tserkovnyak", "Eric Hudson"
+    ],
+    "UIUC Physics": [
+        "Paul Kwiat", "Nadya Mason", "Karin Dahmen", "Dale Van Harlingen"
+    ],
+    "UMich Physics": [
+        "Emanuel Gull", "Xiaoming Mao", "Sharon Glotzer", "Kai Sun", "Mark Newman"
+    ],
+    "UT Austin Physics": [
+        "Scott Aaronson", "Elaine Li", "Allan MacDonald", "Nick Hunter-Jones"
+    ],
+    "Duke Physics": [
+        "Matthew Hastings", "Harold Baranger", "Henry Greenside", "Travis Nicholson", "Huanqian Loh"
+    ],
+    "UW Physics": [
+        "Boris Blinov", "Kai-Mei Fu", "Ting Cao"
+    ],
+    "UMD Physics": [
+        "Chris Monroe", "Norbert Linke", "Victor Galitski", "Kartik Srinivasan"
+    ],
+    "Georgia Tech Physics": [
+        "Predrag Cvitanovic", "Michael Chapman", "Colin Parker"
+    ]
+}
+
+# --- CS EXPANSION (Theory, Systems, Programming Languages) ---
+CS_GENERAL_FACULTY = {
+    "MIT CS Theory/Systems": [
+        "Frans Kaashoek", "Robert Morris", "Nickolai Zeldovich", "Adam Belay",
+        "Henry Corrigan-Gibbs", "Vinod Vaikuntanathan", "Yael Kalai"
+    ],
+    "Stanford CS Theory/Systems": [
+        "John Ousterhout", "Mendel Rosenblum", "Philip Levis", "Christos Kozyrakis",
+        "Alex Aiken", "Mary Wootters", "Aaron Sidford"
+    ],
+    "Berkeley CS Theory/Systems": [
+        "Sylvia Ratnasamy", "John Kubiatowicz", "Joseph Hellerstein", "Raluca Ada Popa",
+        "Koushik Sen", "Max Willsey", "Alvin Cheung", "Christopher Fletcher"
+    ],
+    "CMU CS Theory/Systems": [
+        "David Andersen", "Nathan Beckmann", "Phillip Gibbons", "Rashmi Vinayak",
+        "Bryan Parno", "George Amvrosiadis", "Stephanie Balzer", "Maria Balcan"
+    ],
+    "Princeton CS Theory/Systems": [
+        "Wyatt Lloyd", "Kyle Jamieson", "Ran Raz", "Huacheng Yu",
+        "Zachary Kincaid"
+    ],
+    "Cornell CS Theory/Systems": [
+        "Robbert van Renesse", "Nate Foster", "Dexter Kozen", "Robert Kleinberg",
+        "Ken Birman", "Christopher Batten"
+    ],
+    "UW CS Theory/Systems": [
+        "Tom Anderson", "Arvind Krishnamurthy", "Dan Grossman", "Zachary Tatlock",
+        "Simon Peter", "Paul Beame", "Anna Karlin", "Shayan Oveis Gharan",
+        "Mark Oskin", "Michael Bedford Taylor"
+    ],
+    "UT Austin CS Theory/Systems": [
+        "Vijay Chidambaram", "Christopher Rossbach", "Emmett Witchel",
+        "David Zuckerman", "Calvin Lin"
+    ],
+    "UIUC CS Theory/Systems": [
+        "Grigore Rosu", "Madhusudan Parthasarathy", "Kevin Chang", "Bill Gropp", "Jeff Erickson"
+    ],
+    "Georgia Tech CS Theory/Systems": [
+        "Alexandros Daglis", "Thomas Conte", "Umakishore Ramachandran"
+    ],
+    "UCLA CS Theory/Systems": [
+        "Jens Palsberg", "Harry Xu", "Remy Wang"
+    ],
+    "UMich CS Theory/Systems": [
+        "Satish Narayanasamy", "Yatin Manerkar", "George Tzimpragos",
+        "Max New", "Cyrus Omar", "Manos Kapritsos"
+    ],
+    "Columbia CS Theory/Systems": [
+        "Asaf Cidon", "Junfeng Yang", "Xi Chen"
+    ],
+    "Harvard CS Theory/Systems": [
+        "Minlan Yu", "Juncheng Yang", "Madhu Sudan", "Boaz Barak", "Nada Amin"
+    ],
+    "Yale CS Theory/Systems": [
+        "Bryan Ford", "Yang Richard Yang", "Avi Silberschatz"
+    ],
+    "Brown CS Theory/Systems": [
+        "Tim Nelson", "Nikos Vasilakis", "Kathi Fisler", "Will Crichton"
+    ],
+    "UPenn CS Theory/Systems": [
+        "Andre DeHon", "Joe Devietti", "Sebastian Angel", "Boon Thau Loo"
+    ],
+    "Duke CS Theory/Systems": [
+        "Alvin Lebeck", "Landon Cox", "Bruce Maggs"
+    ],
+    "Northwestern CS Theory/Systems": [
+        "Simone Campanoni", "Jason Hartline", "Aravindan Vijayaraghavan", "Sidhanth Mohanty"
+    ],
+    "Purdue CS Theory/Systems": [
+        "Tiark Rompf", "Milind Kulkarni", "Samuel Midkiff", "Mithuna Thottethodi"
+    ],
+    "UCSD CS Theory/Systems": [
+        "Hadi Esmaeilzadeh", "Jishen Zhao", "Ranjit Jhala"
+    ],
+    "UMass CS Theory/Systems": [
+        "Arjun Guha", "Andrew McGregor"
+    ],
+    "UMD CS Theory/Systems": [
+        "Michael Hicks", "Jeffrey Foster", "Neil Spring", "Samir Khuller"
+    ],
+    "Wisconsin CS Theory/Systems": [
+        "Michael Swift", "Dileep Kini"
+    ],
+    "Caltech CS Theory/Systems": [
+        "Urmila Mahadev", "John Ledyard"
+    ]
+}
+
+# --- INFORMATION SCHOOLS (Computational/Quantitative Information Science) ---
+INFORMATICS_FACULTY = {
+    "Berkeley iSchool": [
+        "David Bamman", "Joshua Blumenstock", "Marti Hearst", "Hany Farid",
+        "Coye Cheshire", "Deirdre Mulligan", "Niloufar Salehi", "Steven Weber",
+        "Alistair Croll", "AnHai Doan", "Morgan Ames", "Nick Merrill"
+    ],
+    "UMich SI": [
+        "Christopher Brooks", "Ceren Budak", "Kevyn Collins-Thompson", "Abigail Jacobs",
+        "David Jurgens", "Matthew Kay", "Paul Resnick", "Daniel Romero",
+        "Sarita Schoenebeck", "Andrea Thomer", "Vinod Vydiswaran", "Libby Hemphill"
+    ],
+    "UW iSchool": [
+        "Bill Howe", "Jevin West", "Anind K. Dey", "Amy J. Ko", "Tanu Mitra",
+        "Katie Davis", "Cecilia Aragon", "Jacob O. Wobbrock", "Aylin Caliskan",
+        "Ben Lee", "Martin Saveski", "Emma Spiro", "Melanie Walsh", "Nicholas Weber",
+        "Lindah Kotut", "Lucy Lu Wang", "Chirag Shah", "Alexis Hiniker"
+    ],
+    "Cornell IS": [
+        "Michael Macy", "Cristian Danescu-Niculescu-Mizil", "Shiri Azenkot",
+        "Solon Barocas", "Larry Blume", "Cristobal Cheyre", "Tanzeem Choudhury",
+        "Abe Davis", "Nicola Dell", "David Easley", "Deborah Estrin", "Nikhil Garg"
+    ],
+    "UIUC iSchool": [
+        "Catherine Blake", "J. Stephen Downie", "Jingrui He", "Christopher Lueg",
+        "Eunice E. Santos", "Ted Underwood", "Yang Wang", "Dong Wang", "Nigel Bosch",
+        "Ryan Cordell", "Yun Huang", "Matthew Turk", "Vetle Torvik", "Kahyun Choi",
+        "Yonghan Jung", "Jiaqi Ma", "Haohan Wang"
+    ],
+    "Maryland iSchool": [
+        "Douglas Oard", "Joel Chan", "Vanessa Frias-Martinez", "Brian Butler",
+        "Wei Ai", "Christopher Antoun", "Eun Kyoung Choe", "Hernisa Kacorri",
+        "Jennifer Preece", "Ben Shneiderman"
+    ],
+    "UT Austin iSchool": [
+        "Matthew Lease", "Ying Ding", "Jacek Gwizdka", "Elliott Hauser",
+        "James Howison", "Chan Park", "Jiaxin Pei", "Nathan TeBlunthuis",
+        "Yan Zhang", "Min Kyung Lee"
+    ],
+    "Indiana Luddy": [
+        "Filippo Menczer", "David Wild", "Patrick Shih", "Karl MacDorman",
+        "Andrew Miller", "Stephen Downs", "Samantha Wood", "Katy Börner",
+        "Xiaozhong Liu", "Yong-Yeol Ahn"
+    ],
+    "UNC SILS": [
+        "Diane Kelly", "Gary Marchionini", "Ryan Shaw", "Stephanie Haas",
+        "Arcot Rajasekar", "Jeffrey Bardzell", "Ronald Bergquist", "Jaime Arguello",
+        "Brad Hemminger", "Yongjun Zhu"
+    ],
+    "Syracuse iSchool": [
+        "Bei Yu", "Jennifer Stromer-Galley", "Jeffrey Saltz", "Sevgi Erdogan",
+        "Yiqi Li", "Kelvin King", "Md Tariqul Islam", "Jasmina Tacheva",
+        "Steve Sawyer", "Kevin Crowston"
+    ],
+    "Rutgers SCI": [
+        "Vivek K. Singh", "Eugene Lee", "Amelia Acker", "Ali Motamedi",
+        "Marie Radford", "Dafna Lemish", "Michael Lesk", "Tefko Saracevic"
+    ],
+    "Drexel CCI": [
+        "Jane Greenberg", "Xia Lin", "Erjia Yan", "Tim Gorichanaz",
+        "Michelle Rogers", "Chaim Zins", "Jake Williams", "Yuan An"
+    ]
+}
+
 # Combine all faculty into one dictionary for the fetch script
 FACULTY_BY_SCHOOL = {}
 FACULTY_BY_SCHOOL.update(CS_FACULTY)
 FACULTY_BY_SCHOOL.update(STATS_FACULTY)
 FACULTY_BY_SCHOOL.update(DS_FACULTY)
 FACULTY_BY_SCHOOL.update(MATH_FACULTY)
+FACULTY_BY_SCHOOL.update(MATH_GENERAL_FACULTY)
+FACULTY_BY_SCHOOL.update(PHYSICS_FACULTY)
+FACULTY_BY_SCHOOL.update(CS_GENERAL_FACULTY)
+# INFORMATICS_FACULTY removed - replaced with Economics/Econometrics expansion
 
 
 def search_author(name: str) -> dict | None:
